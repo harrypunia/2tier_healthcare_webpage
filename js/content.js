ab11e716b84a7c2dc93761609ab088f47fb66ae5
Harry <harryrock67@gmail.com>
2019-04-06 17:00:10 -0400

    added basic tempalte for sccs and html

---
+++ js/content.js
@@ -0,0 +1,5 @@
+/*Logo Text*/     parseContent("Logo name", "header__logo__text");
+/*Header Item 1*/ parseContent("Menu", "header__item-1");
+/*Header Item 2*/ parseContent("About", "header__item-2");
+/*Header Item 3*/ parseContent("Ads", "header__item-3");
+/*Header Item 4*/ parseContent("Videos", "header__item-4");
---
+++ js/main.js
@@ -0,0 +1,1 @@
+const parseContent = (text, className) => document.getElementsByClassName(className)[0].innerHTML = text;
