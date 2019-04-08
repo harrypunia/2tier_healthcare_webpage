3ce185e7b86cda5e3e703ed4670391ae7b0cee77
Harry <harryrock67@gmail.com>
2019-04-07 23:59:25 -0400

    added multople sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+let ads = document.getElementsByClassName('ads__item');
+let preview = document.getElementsByClassName('ads__preview')[0];
+let personas = document.getElementsByClassName('personas__crousel')[0].getElementsByTagName('img');
+let displayPersona = document.getElementsByClassName('personas__info')[0];
+
+for(let i = 0; i < ads.length; i++) {
+    ads[i].addEventListener("click", e => {
+        let img = e.currentTarget.getElementsByTagName("img")[0];
+        openPreview(img);
+    })
+}
+
+for(let i = 0; i < personas.length; i++) {
+    personas[i].addEventListener('click', e => {
+        let data = e.currentTarget.getAttribute('data');
+        displayPersona.setAttribute('src',  'assets/img/' + data + '.png');
+        for(let j = 0; j < personas.length; j++) {personas[j].classList.remove('active')}
+        personas[i].classList.add('active');
+    })
+}
+
+const openPreview = img => {    
+    preview.style.display = 'block';
+    preview.getElementsByTagName('img')[0].setAttribute('src', img.src);
+}
+
+const closePreview = () => {
+    preview.style.display = 'none';
+    preview.getElementsByTagName('img')[0].setAttribute('src', '');
+}
+
 const parseContent = (text, className) => document.getElementsByClassName(className)[0].innerHTML = text;
